2c5de636e83c8154a5e700ccdd6784fcbdc6fdab
RogueInk <135592855+RogueInk@users.noreply.github.com>
2023-06-08 15:58:38 +0530

    Update RogueHub.lua

--- RogueHub.lua
+++ RogueHub.lua
@@ -1,11 +1,12 @@
+repeat wait() until game:IsLoaded()
 local player = game:GetService("Players").LocalPlayer
 
-getgenv().bandagebuy = true
-getgenv().bandagesell = true
-getgenv().antiidle = true
-getgenv().autospin = true
+getgenv().bandagebuy = false
+getgenv().bandagesell = false
+getgenv().antiidle = false
+getgenv().autospin = false
 getgenv().speed = 0.1
-getgenv().dailyspin = true
+getgenv().dailyspin = false
 
 
     function getNumber()
@@ -23,7 +24,6 @@ getgenv().dailyspin = true
 
 
 function bandagebuyer()
-    while bandagebuy == true do
         local args = {
             [1] = "buysomething",
             [2] = game:GetService("Players").LocalPlayer,
@@ -36,13 +36,11 @@ function bandagebuyer()
     game:GetService("ReplicatedStorage").Remotes.To_Server.Handle_Initiate_S:FireServer(unpack(args))
     task.wait()
 
-    end
-
 end
 
 function AntiAfk()
-    while antiidle == true do
-
+    while antiidle do
+        task.wait()
         local vu = game:GetService("VirtualUser")
 
         game:GetService("Players").LocalPlayer.Idled:connect(function()
@@ -67,110 +65,150 @@ local targetclan = {
             local args = {[1] = "check_can_spin"}
             game:GetService("ReplicatedStorage").Remotes.To_Server.Handle_Initiate_S:FireServer(unpack(args))
             wait(getgenv().speed)
+            task.wait()
         end
     end
 end
 
 
+ 
 
 
 
 
+local player = game:GetService("Players").LocalPlayer
+function bandagebuyer()
+    while bandagebuy == true do
+        local args = {
+            [1] = "buysomething",
+            [2] = game:GetService("Players").LocalPlayer,
+            [3] = "Bandage",
+            [4] = game:GetService("ReplicatedStorage").Player_Data[player.Name].Yen,
+            [5] = game:GetService("ReplicatedStorage").Player_Data[player.Name].Inventory,
+            [6] = 5
+    }
 
+    game:GetService("ReplicatedStorage").Remotes.To_Server.Handle_Initiate_S:FireServer(unpack(args))
+    task.wait()
 
-
-
-
-local library = loadstring(game:HttpGet(('https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wall%20v3')))()
-
-local w = library:CreateWindow("Rogue Hub") -- Creates the window
-local b = w:CreateFolder("Millionaire Farm")
-local c = w:CreateFolder("Teleport to -")
-local d = w:CreateFolder("Auto Thingy")
-
-b:Toggle("Buy",function(bool)
-    getgenv().bandagebuy = bool
-    if bool then -- This won't spam because u put if instead of do
-        bandagebuyer()
     end
-end)
+
+end
+
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+getgenv().SecureMode = true
 
 
-b:Toggle("Sell",function(bool)
-    getgenv().bandagesell = bool
+local Window = Rayfield:CreateWindow({
+    Name = "Key System",
+    LoadingTitle = "Rogue Hub",
+    LoadingSubtitle = "by RogueInk",
+    ConfigurationSaving = {
+       Enabled = true,
+       FolderName = "Rogue Hub", -- Create a custom folder for your hub/game
+       FileName = "Project Slayer"
+    },
+    Discord = {
+       Enabled = true,
+       Invite = "vrYUGtVePK", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+       RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+    },
+    KeySystem  = true, -- Set this to true to use our key system
+    KeySettings = {
+       Title = "Rogue Hub",
+       Subtitle = "Key System",
+       Note = "Join Discord for key",
+       FileName = "Key", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+       SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+       GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+       Key = {"Hello"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+    }
+ })
+
+
+
+
+local Tab = Window:CreateTab("MISC", 4483362458) -- Title, Image
+local Section = Tab:CreateSection("Millionaire Farm (Bandage)")
+local Toggle = Tab:CreateToggle({
+    Name = "Auto Buy",
+    CurrentValue = false,
+    Callback = function(Value)
+        bandagebuy = Value
+        while Value do
+            bandagebuyer()
+            task.wait()
+        end
+    -- The function that takes place when the toggle is pressed
+    -- The variable (Value) is a boolean on whether the toggle is true or false
+    end,
+ })
+
+ local Toggle = Tab:CreateToggle({
+    Name = "Auto Sell",
+    CurrentValue = false,
+    Callback = function(Value)
+        bandagesell = Value
         while bandagesell do
             task.wait()
             print()
-        local ohTable1 = {
-            [getNumber()] = 1
-        }
-        local ohNumber2 = 0
-        local ohNumber3 = 0
+            local ohTable1 = {
+                [getNumber()] = 1
+            }
+            local ohNumber2 = 0
+            local ohNumber3 = 0
 
         game:GetService("ReplicatedStorage").Sell_Items_tang:InvokeServer(ohTable1, ohNumber2, ohNumber3)
+        task.wait()
 
         end
-end)
+    end,
+ })
+ local Section = Tab:CreateSection("EXTRA")
+ local Toggle = Tab:CreateToggle({
+     Name = "Anti AFK",
+     CurrentValue = false,
+     Callback = function(Value)
+         antiidle = Value
+         while Value do
+             AntiAfk()
+             task.wait()
+         end
+     -- The function that takes place when the toggle is pressed
+     -- The variable (Value) is a boolean on whether the toggle is true or false
+     end,
+  })
 
 
-d:Button("Anti Afk",function()
-    AntiAfk()
-    print("Anti Afk Started now go away", antiidle)
-end)
-
-d:Button("Daily Spin",function()
-    while dailyspin == true do
-        game:GetService("ReplicatedStorage").spins_thing_remote:InvokeServer()
-    end
-end)
-
-
-d:Button("Kamdo spinner",function()
-    KamadoSpin()
-    print("We spinin KAMADO for ya")
-end)
-d:DestroyGui()
+  local Toggle = Tab:CreateToggle({
+    Name = "Spin Till Kamado",
+    CurrentValue = false,
+    Callback = function(Value)
+        autospin = Value
+        while Value do
+             KamadoSpin()
+             task.wait()
+        end
+    -- The function that takes place when the toggle is pressed
+    -- The variable (Value) is a boolean on whether the toggle is true or false
+    end,
+ })
 
 
+ local Toggle = Tab:CreateToggle({
+    Name = "Daily Spin",
+    CurrentValue = false,
+    Callback = function(Value)
+        dailyspin = Value
+        while Value do
+            game:GetService("ReplicatedStorage").spins_thing_remote:InvokeServer()
+            task.wait()
+        end
+    -- The function that takes place when the toggle is pressed
+    -- The variable (Value) is a boolean on whether the toggle is true or false
+    end,
+ })
 
 
-
-
-
-
-
-
-
-
-
-
--- b:Label("Auto Buy Bandage",{
---     TextSize = 25; -- Self Explaining
---     TextColor = Color3.fromRGB(255,255,255); -- Self Explaining
---     BgColor = Color3.fromRGB(69,69,69); -- Self Explaining
-    
--- }) 
-
--- b:Slider("Slider",{
---     min = 10; -- min value of the slider
---     max = 50; -- max value of the slider
---     precise = true; -- max 2 decimals
--- },function(value)
---     print(value)
--- end)
-
--- b:Dropdown("Dropdown",{"A","B","C"},true,function(mob) --true/false, replaces the current title "Dropdown" with the option that t
---     print(mob)
--- end)
-
--- b:Bind("Bind",Enum.KeyCode.C,function() --Default bind
---     print("Yes")
--- end)
-
--- b:ColorPicker("ColorPicker",Color3.fromRGB(255,0,0),function(color) --Default color
---     print(color)
--- end)
-
--- b:Box("Box","number",function(value) -- "number" or "string"
---     print(value)
--- end) 
+---Rayfield:Destroy()
+Rayfield:LoadConfiguration()
